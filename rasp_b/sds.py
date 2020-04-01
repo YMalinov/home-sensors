dd4a930418f7266cfb766f956f34edd5e249f4f0
Yanko Malinov <yanko.malinov@gmail.com>
2020-04-01 15:13:06 +0300

    Increase SDS011 rather optimistic read timeout and add logs for easy debugging

--- rasp_b/sds.py
+++ rasp_b/sds.py
@@ -12,32 +12,38 @@ from sds011 import SDS011
 DEVICE_FILE = '/dev/ttyUSB0' # let's hope it stays there
 BAUD_RATE = 9600
 
-# As in, how long are we gonna wait to collect a full response from the device.
-READ_TIMEOUT = 4 # in secs;
+# Sensor is sometimes slow, hence the generous read timeout:
+READ_TIMEOUT = 20 # in secs
+
+# As in, how long are we gonna wait before collecting the sensors' readings:
 WARMUP_PERIOD = 120 # in secs
 
 def get():
+    print('Initiating connection with SDS011...')
     sensor = SDS011(
         DEVICE_FILE,
         timeout=READ_TIMEOUT,
         unit_of_measure=SDS011.UnitsOfMeasure.MassConcentrationEuropean
     )
 
+    print('Resetting...')
     sensor.reset()
 
     # Let's wake the sensor up...
+    print('Waking up...')
     sensor.workstate = SDS011.WorkStates.Measuring
 
     # ...and leave it some time to get qualified readings.
     print('Going to sleep for %d secs...' % WARMUP_PERIOD)
     time.sleep(WARMUP_PERIOD)
-    print('Done sleeping, next: read sensors...')
 
     # Should be enough. Get the values and...
+    print('Done sleeping, next: read SDS011 measurements...')
     values = sensor.get_values()
 
     # ... put it to sleep until next time we need its values. It has a
     # mechanical fan - better not keep it spinning needlessly.
+    print('Done! Putting sensor back to sleep...')
     sensor.workstate = SDS011.WorkStates.Sleeping
 
     return {
