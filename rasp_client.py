527f714809614d14b22c4e3e64c1a75ef72e4f65
Yanko Malinov <yanko.malinov@gmail.com>
2020-04-05 13:21:19 +0300

    Save client readings to file even if server is unreachable

--- rasp_client.py
+++ rasp_client.py
@@ -20,9 +20,12 @@ def post_update(sensor_data):
     # them might also be a long distance from the router. As a result, they
     # might not always be able to post their updates on the first try, so try
     # several times until we get a positive response from the server.
+    last_res = None
     tries = 5
     for i in range(tries):
-        res = requests.post(backend_url, json = data)
-        if res.status_code == 202:
-            common.print_to_file(readings + str(res), last_file)
+        last_res = requests.post(backend_url, json = data)
+        if last_res.status_code == 202:
+            last_res = f'{last_res} on try {i + 1}\n'
             break
+
+    common.print_to_file(readings + last_res, last_file)
