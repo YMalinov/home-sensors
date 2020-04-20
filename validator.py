d0f92f8f1525e1a936fe18144d50d958d5451380
Yanko Malinov <yanko.malinov@gmail.com>
2020-04-20 21:38:22 +0300

    Refactor validator.py

--- validator.py
+++ validator.py
@@ -1,14 +1,13 @@
 from common import Sensor
 
-# generic "is number" check
-def try_parse_float(input_num):
+# Generic "is number" check
+def __try_parse_float(input_num):
     try:
-        float(input_num)
-        return True
+        return float(input_num)
     except ValueError:
         return False
 
-def __temperature__(value):
+def __temperature(value):
     # The DS18B20 sensor supports a temperature reading range of [-55, 125] °C.
     # BME280's temperature sensor has a narrower range, so we're fine.
     if -55 <= value <= 125:
@@ -16,7 +15,7 @@ def __temperature__(value):
 
     return False
 
-def __pressure__(value):
+def __pressure(value):
     # The BME280 sensor (which I'm using for pressure) supports a pressure
     # measurement range of [300, 1100] hPa.
     if 300 <= value <= 1100:
@@ -24,7 +23,7 @@ def __pressure__(value):
 
     return False
 
-def __humidity__(value):
+def __humidity(value):
     # Percentage of the humidity in the air. As such, it can only be between
     # 0 and 100%.
     if 0 <= value <= 100:
@@ -32,7 +31,7 @@ def __humidity__(value):
 
     return False
 
-def __pm__(value):
+def __pm(value):
     # The SDS011 supports a PM (Particulate Matter) reading range of
     # [0.0, 999.9] μg/m3.
     if 0 <= value <= 1000:
@@ -44,19 +43,18 @@ def is_sane(sensor, value):
     if not isinstance(sensor, Sensor):
         raise TypeError('sensor isn\'t a common.Sensor')
 
-    if not try_parse_float(value):
+    reading = __try_parse_float(value)
+    if not reading:
         return False
 
-    reading = float(value)
-
     mapper = {
-        Sensor.ds18_long_temp: __temperature__,
-        Sensor.ds18_short_temp: __temperature__,
-        Sensor.bme_temp: __temperature__,
-        Sensor.bme_pressure: __pressure__,
-        Sensor.bme_humidity: __humidity__,
-        Sensor.sds_pm25: __pm__,
-        Sensor.sds_pm10: __pm__,
+        Sensor.ds18_long_temp: __temperature,
+        Sensor.ds18_short_temp: __temperature,
+        Sensor.bme_temp: __temperature,
+        Sensor.bme_pressure: __pressure,
+        Sensor.bme_humidity: __humidity,
+        Sensor.sds_pm25: __pm,
+        Sensor.sds_pm10: __pm,
     }
 
     return mapper[sensor](reading)
